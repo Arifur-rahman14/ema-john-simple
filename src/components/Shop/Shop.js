75eb1dab2012af28ed6cfe04d7eb75d9a3170c96
Arifur-rahman14 <aarifrahh@yahoo.com>
2021-04-20 17:03:49 +0600

    data loaded

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import fakeData from '../../fakeData';
+import Product from '../Product/Product';
+import './Shop.css';
+
+const Shop = () => {
+    const first10 = fakeData.slice(0,20);
+    const [products, setProducts] = useState(first10);
+
+    const handleAddProduct = (product) =>{
+        console.log("product added", product);
+    }
+    return (
+        <div className="shop-container">
+            <div className="product-container"> 
+                {
+                    products.map(pd => 
+                        <Product handleAddProduct = {handleAddProduct} 
+                        product={pd}>
+                            
+                        </Product>)
+                }
+               
+            </div>
+            <div className="cart-container">
+                <h3>this is cart</h3>
+                <h4>how areyou man</h4>
+            </div>
+            
+            
+        </div>
+    );
+};
+
+export default Shop;
